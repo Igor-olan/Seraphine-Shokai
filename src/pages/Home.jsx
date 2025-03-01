c64c3542b0195209b87df96c0d7d162582496fa6
Igor-Olan <olangan4238q@gmail.com>
2025-03-01 15:53:21 +0700

    add vercel.json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import Hero from "../components/Hero/Hero";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Release from "../components/Update/Release.jsx";
-// import Footer from "../components/Footer/Footer";
 
 const App = () => {
 
@@ -22,9 +21,7 @@ const App = () => {
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200"> 
       <Hero />
       {/* <Banner /> */}
-      {/* <CoverBanner /> */}
       <Release />
-      {/* <Footer /> */}
     </div>
   );
 };
